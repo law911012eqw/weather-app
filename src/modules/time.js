cc31384b6c3b8a430fc9113db49611b9a1852f0b
Beaver Bryan Antipolo <beaverbryanantipolo@gmail.com>
2021-01-26 21:27:55 -0500

    Categorically effectuate the appropriate date/time format on each weather data

---
+++ src/modules/time.js
@@ -0,0 +1,39 @@
+//convert dates by the appropriate local timezone
+//object used to format dates
+const OPTIONS = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+function convertTZ(date, tzString) {
+    return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));   
+}
+
+//format -> HH:MM:SS AM/PM
+function formatHourMin(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = ('0'+minutes).slice(-2);
+    seconds = ('0'+seconds).slice(-2);
+    let strTime = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+    return strTime;
+}
+
+//format -> HH AM/PM
+function formatHourOnly(date, i) {
+    let hours = date.getHours() + i;
+    let hoursNextDay = (hours-24); //change the variable if is passed 11pm
+    hours > 23 ? hours = 0 + hoursNextDay : hours;
+    let ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    let strTime = hours + ' ' + ampm;
+    return strTime;
+}
+
+function formatFullDate(date) {
+    return date.toLocaleDateString("en-US", OPTIONS);
+}
+
+export { convertTZ, formatHourMin, formatHourOnly, formatFullDate }
