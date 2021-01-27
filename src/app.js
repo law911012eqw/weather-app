37e20949b9564bab5e88db244b1099a1f55298de
Beaver Bryan Antipolo <beaverbryanantipolo@gmail.com>
2021-01-27 17:00:08 -0500

    Chore and style: Update webpack config and implementation of existing features

--- src/app.js
+++ src/app.js
@@ -1,221 +1,225 @@
 import fetchData from './modules/fetch_data.js';
 import DOM from './modules/elements.js';
 import manage from './modules/helpers.js';
-import { kelvinToCelcius, kelvinToFahrenheit, windSpeedToMPH, windDegToDir } from './modules/units.js';
+import { convertTemp, windSpeedToMPH, windDegToDir } from './modules/units.js';
 import { convertTZ, formatHourMin, formatHourOnly, formatFullDate } from './modules/time.js';
 import { getAreaFromCoordinates } from './modules/current_location.js';
+import { getAlert, toggleDisplay } from './modules/alert.js';
 import './styles/reset.css';
 import './styles/style.css';
+import './styles/alert.css';
 
-//weather variables
+// the only global variable
 let weatherData;
-let location;
-let isTempCelcius = false;
 
-//ask the user for a permission to collect the former's coordinates
+// ask the user for a permission to collect the former's coordinates
 window.onload = () => {
-    defaultLocation();
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(assignUserLocation);
-    } else {;
-        console.log('Geolocation is not supported by this browser');
-    }
-}
+	manage.modifyAttr(DOM.modal, 'style', 'display: block;');
+	defaultLocation();
+	if (navigator.geolocation) {
+		manage.modifyAttr(DOM.modal, 'style', 'display: none;');
+		navigator.geolocation.getCurrentPosition(assignUserLocation);
+	} else {
+		manage.modifyAttr(DOM.modal, 'style', 'display: none;');
+		console.log('Geolocation is not supported by this browser');
+	}
+};
 
-//assign the user's location as current
+// assign the user's location as current
 async function assignUserLocation(pos) {
-    const lat = pos.coords.latitude;
-    const lon = pos.coords.longitude;
-    fetchData.setCoordinates(lat,lon);
-    const data = await getAreaFromCoordinates({lat,lon});
-    let areaName = data.osmtags.wikipedia;
-    if (areaName.indexOf(':') !== -1) {
-        areaName = areaName.split(':')[1];
-      }
-    fetchData.setAreaName(areaName);
-    console.log(data);
-    try{
-        await fetchWeatherData(fetchData.getWeatherURL());
-    } catch(e) {
-        console.log(`Error: ${e}`);
-    }
+	const lat = pos.coords.latitude;
+	const lon = pos.coords.longitude;
+	fetchData.setCoordinates(lat, lon);
+	const data = await getAreaFromCoordinates({ lat, lon });
+	let areaName = data.osmtags.wikipedia;
+	if (areaName.indexOf(':') !== -1) {
+		areaName = areaName.split(':')[1];
+	}
+	fetchData.setAreaName(areaName);
+	console.log(data);
+	try {
+		await fetchWeatherData(fetchData.getWeatherURL());
+	} catch (e) {
+		console.log(`Error: ${e}`);
+	}
 }
 
-//use default location if the user disallow the humble permission
+// use default location if the user disallow the humble permission
 async function defaultLocation() {
-    const lat = 90;
-    const lon = 0;
-    fetchData.setCoordinates(lat,lon);
-    fetchData.setAreaName('North Pole');
-    const data = await fetchData.getWeatherURL();
-    console.log(fetchData.getAreaName());
-    console.log(data);
-    try {
-        await fetchWeatherData(data);
-    } catch(e) {
-        console.log(`Error: ${e}`);
-    }
+	const lat = 90;
+	const lon = 0;
+	fetchData.setCoordinates(lat, lon);
+	fetchData.setAreaName('North Pole');
+	const data = await fetchData.getWeatherURL();
+	console.log(fetchData.getAreaName());
+	console.log(data);
+	try {
+		await fetchWeatherData(data);
+	} catch (e) {
+		console.log(`Error: ${e}`);
+	}
 }
 
-//submit the form to get the input of coordinates and overwrite to the respective
-//variables, appended within the weather url.
-DOM.form.onsubmit = async(e) => {
-    e.preventDefault();
-    fetchData.setSearchArea(DOM.locationInput());
-    fetchData.setAreaName(DOM.strInput());
-    try {
-        DOM.debug.textContent = '';
-        await getCoordinates(fetchData.getLocationURL());
-    }
-    catch(error) {
-        DOM.debug.textContent = 'This location doesn\'t exist!!';
-        console.log(error);
-    }
-}
+// submit the form to get the input of coordinates and overwrite to the respective
+// variables, appended within the weather url.
+DOM.form.onsubmit = async (e) => {
+	e.preventDefault();
+	fetchData.setSearchArea(DOM.locationInput());
+	fetchData.setAreaName(DOM.strInput());
+	try {
+		await getCoordinates(fetchData.getLocationURL());
+	}
+	catch (error) {
+		console.log(error);
+	}
+};
 
-//get the coordinates
+DOM.alertClose.addEventListener('click', toggleDisplay);
+DOM.alertBtn.addEventListener('click', toggleDisplay);
+// get the coordinates
 async function getCoordinates(data) {
-    const response = await fetch(data, {mode: 'cors'});
-    const locationData = await response.json();
-    fetchData.setCoordinates(locationData[0].lat,locationData[0].lon);
-    console.log(fetchData.getAreaName());
-    try {
-        await fetchWeatherData(fetchData.getWeatherURL());
-    }
-    catch(e) {
-        console.log(`Error: ${e}`);
-    }
+	const response = await fetch(data, { mode: 'cors' });
+	const locationData = await response.json();
+	fetchData.setCoordinates(locationData[0].lat, locationData[0].lon);
+	console.log(fetchData.getAreaName());
+	try {
+		await fetchWeatherData(fetchData.getWeatherURL());
+	}
+	catch (e) {
+		console.log(`Error: ${e}`);
+	}
 }
 
-//fetch the data and display it 
+// fetch the data and display it
 async function fetchWeatherData(data) {
-    const response = await fetch(data,{mode: 'cors'});
-    weatherData = await response.json();
-    console.log(fetchData.getWeatherURL());
-    console.log(fetchData.getAreaName());
-    displayInfo();
+	const response = await fetch(data, { mode: 'cors' });
+	weatherData = await response.json();
+	console.log(fetchData.getWeatherURL());
+	console.log(fetchData.getAreaName());
+	displayInfo();
 }
 
 function displayInfo() {
-    main.textContent = '';
-    const upperMain = manage.crtElement('','upper-main','weather-container','div');
-    const lowerMain = manage.crtElement('','lower-main','weather-container','div');
-    const currentMain = manage.crtElement('','current-main','weather-container','div');
-    const dailyMain = manage.crtElement('','daily-main','weather-container','div');
-    DOM.main.append(upperMain, lowerMain);
-    upperMain.append(currentMain, dailyMain);
-    getHourlyData(lowerMain);
-    //getDailyData(dailyMain);
-    displayCurrentOutput(currentMain);
+	DOM.main.textContent = '';
+	const upperMain = manage.crtElement('', 'upper-main', 'weather-container', 'div');
+	const lowerMain = manage.crtElement('', 'lower-main', 'weather-container', 'div');
+	const currentMain = manage.crtElement('', 'current-main', 'weather-container', 'div');
+	const dailyMain = manage.crtElement('', 'daily-main', 'weather-container', 'div');
+	DOM.main.append(upperMain, lowerMain);
+	upperMain.append(currentMain, dailyMain);
+	getHourlyData(lowerMain);
+	// getDailyData(dailyMain);
+	displayCurrentOutput(currentMain);
 }
 
-const getHourlyData = async (parent) => {
-    for(let i = 0; i < 9; i++) {
-        await displayHourlyOutput(i,parent);
-    } 
-}
+const getHourlyData = (parent) => {
+	for (let i = 0; i < 9; i++) {
+		displayHourlyOutput(i, parent);
+	}
+};
 
-//display hourly contents 
-function displayHourlyOutput(i,parent) {
-    //temperature data from current object
-    let temp = weatherData.hourly[(i+1)+i].temp;
-    let feels_like = weatherData.hourly[(i+1)+i].feels_like;
-        
-    const icon = weatherData.hourly[(i+1)+i].weather[0].icon;
-    const humidity = weatherData.hourly[(i+1)+i].humidity + '%';
-    const timezone = weatherData.timezone;
-    const date = convertTZ(new Date,timezone)
+// display hourly contents
+function displayHourlyOutput(i, parent) {
+	// temperature data from current object
+	let temp = weatherData.hourly[(i + 1) + i].temp;
+	let feels_like = weatherData.hourly[(i + 1) + i].feels_like;
 
-    temp = convertTemp(temp);
-    feels_like = convertTemp(feels_like);
+	const icon = weatherData.hourly[(i + 1) + i].weather[0].icon;
+	const humidity = weatherData.hourly[(i + 1) + i].humidity + '%';
+	const timezone = weatherData.timezone;
+	const date = convertTZ(new Date, timezone);
 
-    //DOM instances
-    const hourlyMain = manage.crtElement('',`hrly-main${i+1}`,'hrly-contents','div');
-    const hourlyTemp = manage.crtElement(`${temp}`,`hrly-temp${i+1}`,'hrly-temp','p');
-    const hourlyFeels = manage.crtElement(`Feels like: ${feels_like}`,`hrly-feels${i+1}`,'hrly-container','p');
-    const hourlyIcon = manage.crtImg(getWeatherIcon(icon),`hrly-icon${i+1}`,'hrly-icons');
-    const hourlyTime = manage.crtElement(formatHourOnly(date, (i+1)+i),`hrly-time${i+1}`,'hrly-time','p');
-    const hourlyHumidity = manage.crtElement(`Humidity: ${humidity}`,`hrly-humidity${i+1}`,'hrly-container','p');
+	temp = convertTemp(temp);
+	feels_like = convertTemp(feels_like);
 
-    //append los elementos de familia 
-    parent.append(hourlyMain);
-    hourlyMain.append(hourlyTime,hourlyTemp,hourlyIcon,hourlyFeels,hourlyHumidity);
+	// DOM instances
+	const hourlyMain = manage.crtElement('', `hrly-main${i + 1}`, 'hrly-contents', 'div');
+	const hourlyTemp = manage.crtElement(`${temp}`, `hrly-temp${i + 1}`, 'hrly-temp', 'p');
+	const hourlyFeels = manage.crtElement(`Feels like: ${feels_like}`, `hrly-feels${i + 1}`, 'hrly-container', 'p');
+	const hourlyIcon = manage.crtImg(getWeatherIcon(icon), `hrly-icon${i + 1}`, 'hrly-icons');
+	const hourlyTime = manage.crtElement(formatHourOnly(date, (i + 1) + i), `hrly-time${i + 1}`, 'hrly-time', 'p');
+	const hourlyHumidity = manage.crtElement(`Humidity: ${humidity}`, `hrly-humidity${i + 1}`, 'hrly-container', 'p');
+
+	// append los elementos de familia
+	parent.append(hourlyMain);
+	hourlyMain.append(hourlyTime, hourlyTemp, hourlyIcon, hourlyFeels, hourlyHumidity);
 }
 
 const getDailyData = async (parent) => {
-    for(let i = 0; i < 6; i++) {
-        await displayDailyOutput(i,parent);
-    } 
-}
+	for (let i = 0; i < 6; i++) {
+		await displayDailyOutput(i, parent);
+	}
+};
 
 // function displayDailyOutput(i,parent){
-// const 
+// const
 // }
 
-//display the weather data about the current hour/day
-function displayCurrentOutput(parent){
-    const icon = weatherData.current.weather[0].icon;
-    const timezone = weatherData.timezone;
-    const date = convertTZ(new Date,timezone)
+// display the weather data about the current hour/day
+function displayCurrentOutput(parent) {
+	const icon = weatherData.current.weather[0].icon;
+	const timezone = weatherData.timezone;
+	const date = convertTZ(new Date, timezone);
 
-    let temp = weatherData.current.temp;
-    let feels = weatherData.current.feels_like;
-    const humidity = weatherData.current.humidity;
-    const speed = `${windSpeedToMPH(weatherData.current.wind_speed)} mph :Wind Speed`;
-    const deg = `${windDegToDir(weatherData.current.wind_deg)} :Wind Degree`;
-    //temp conversion
-    temp = convertTemp(temp); 
-    temp = temp.slice(0, -1)
-    feels = convertTemp(feels);
+	let temp = weatherData.current.temp;
+	let feels = weatherData.current.feels_like;
+	const humidity = weatherData.current.humidity;
+	const speed = `${windSpeedToMPH(weatherData.current.wind_speed)} mph :Wind Speed`;
+	const deg = `${windDegToDir(weatherData.current.wind_deg)} :Wind Degree`;
+	// temp conversion
+	temp = convertTemp(temp);
+	temp = temp.slice(0, -1);
+	feels = convertTemp(feels);
 
-    //DOM instances
-    const currentUprCont = manage.crtElement('','current-upper','current-upper','div');
-    //DOM elements in the upper left side of the current main
-    const upperLeftSide = manage.crtElement('','current-upper-left','current-upper-left','div');
-    const currentLocation = manage.crtElement(fetchData.getAreaName(),'current-location','current-contents','p');
-    const currentDate = manage.crtElement(formatFullDate(date),'current-day','current-contents','p');
-    const currentTime = manage.crtElement(formatHourMin(date),'current-time','current-contents','p');
-    //DOM elements in the upper right side of the current main
-    const upperRightSide = manage.crtElement('','current-upper-right','current-upper-right','div');
-    const currentIcon = manage.crtImg(getWeatherIcon(icon),`current-icon`,'current-contents');
-    const tempCelcius = manage.crtElement(`${String.fromCharCode(176)}C`,'tempC','choose-temp','div');
-    const tempFah = manage.crtElement(`${String.fromCharCode(176)}F`,'tempF','choose-temp','div');
-    //the element that contains the current temperature
-    const currentTemp = manage.crtElement(temp,'current-temp','current-contents','p');
-    //the lower side container
-    const currentLwrCont = manage.crtElement('','current-lower','current-lower','div');
-    //DOM elements in the lower left side of the current main
-    const lowerLeftSide = manage.crtElement('','current-lower-left','current-lower-left','div');
-    const currentFeels = manage.crtElement(`Feels Like: ${feels}`,'current-feels','current-contents','p');
-    const currentHumidity = manage.crtElement(`Humidity: ${humidity}%`,'current-humidity','current-contents','p');
-    //DOM elements in the lower right side of the current main
-    const lowerRightSide = manage.crtElement('','current-lower-right','current-lower-right','div');
-    const currentSpeed = manage.crtElement(speed,'current-speed','current-contents','p')
-    const currentDegree = manage.crtElement(deg,'current-deg','current-contents','p');
+	// DOM instances
+	const currentUprCont = manage.crtElement('', 'current-upper', 'current-upper', 'div');
+	// DOM elements in the upper left side of the current main
+	const upperLeftSide = manage.crtElement('', 'current-upper-left', 'current-upper-left', 'div');
+	const currentLocation = manage.crtElement(fetchData.getAreaName(), 'current-location', 'current-contents', 'p');
+	const currentDate = manage.crtElement(formatFullDate(date), 'current-day', 'current-contents', 'p');
+	const currentTime = manage.crtElement(formatHourMin(date), 'current-time', 'current-contents', 'p');
+	// DOM elements in the upper right side of the current main
+	const upperRightSide = manage.crtElement('', 'current-upper-right', 'current-upper-right', 'div');
+	const currentIcon = manage.crtImg(getWeatherIcon(icon), 'current-icon', 'current-contents');
+	const tempCelcius = manage.crtElement(`${String.fromCharCode(176)}C`, 'tempC', 'choose-temp', 'div');
+	const tempFah = manage.crtElement(`${String.fromCharCode(176)}F`, 'tempF', 'choose-temp', 'div');
+	// the element that contains the current temperature
+	const currentTemp = manage.crtElement(temp, 'current-temp', 'current-contents', 'p');
+	// the lower side container
+	const currentLwrCont = manage.crtElement('', 'current-lower', 'current-lower', 'div');
+	// DOM elements in the lower left side of the current main
+	const lowerLeftSide = manage.crtElement('', 'current-lower-left', 'current-lower-left', 'div');
+	const currentFeels = manage.crtElement(`Feels Like: ${feels}`, 'current-feels', 'current-contents', 'p');
+	const currentHumidity = manage.crtElement(`Humidity: ${humidity}%`, 'current-humidity', 'current-contents', 'p');
+	// DOM elements in the lower right side of the current main
+	const lowerRightSide = manage.crtElement('', 'current-lower-right', 'current-lower-right', 'div');
+	const currentSpeed = manage.crtElement(speed, 'current-speed', 'current-contents', 'p');
+	const currentDegree = manage.crtElement(deg, 'current-deg', 'current-contents', 'p');
 
-    currentLwrCont.append(lowerLeftSide,lowerRightSide);
-    lowerLeftSide.append(currentFeels,currentHumidity);
-    lowerRightSide.append(currentSpeed,currentDegree);
-    currentUprCont.append(upperLeftSide,upperRightSide);
-    upperLeftSide.append(currentLocation,currentDate,currentTime);
-    upperRightSide.append(currentIcon,tempCelcius,tempFah);
-    parent.append(currentUprCont,currentTemp,currentLwrCont);
-
-    updateClock(); //auto refresh clock by 100
+	currentLwrCont.append(lowerLeftSide, lowerRightSide);
+	lowerLeftSide.append(currentFeels, currentHumidity);
+	lowerRightSide.append(currentSpeed, currentDegree);
+	currentUprCont.append(upperLeftSide, upperRightSide);
+	upperLeftSide.append(currentLocation, currentDate, currentTime);
+	upperRightSide.append(currentIcon, tempCelcius, tempFah);
+	parent.append(currentUprCont, currentTemp, currentLwrCont);
+	getAlert(weatherData); // get alert data
+	updateClock(); // auto refresh clock by 100
 }
 
+document.querySelectorAll('choose-temp').addEventListener('click', function(e) {
+	if(e.id == 'tempC'){
+		
+	}
+});
 function getWeatherIcon(icon) {
-    return `http://openweathermap.org/img/wn/${icon}@2x.png`;
+	return `http://openweathermap.org/img/wn/${icon}@2x.png`;
 }
 
 function updateClock() {
-    const timezone = weatherData.timezone;
-    const date = convertTZ(new Date,timezone);
-    const currentTime = document.getElementById(`current-time`);
-    currentTime.textContent = formatHourMin(date);
-    setTimeout(updateClock,1000);
-}
-
-const convertTemp = (t) => {
-    return isTempCelcius === true ? kelvinToCelcius(t) : kelvinToFahrenheit(t);
+	const timezone = weatherData.timezone;
+	const date = convertTZ(new Date, timezone);
+	const currentTime = document.getElementById('current-time');
+	currentTime.textContent = formatHourMin(date);
+	setTimeout(updateClock, 1000);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,71 @@
 const path = require('path');
-const dotenv = require('dotenv');
-const webpack = require('webpack');
+// const Dotenv = require('dotenv-webpack');
+// const dotenv = require('dotenv');
+// const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    app: './src/app.js',
-  },
-  devtool: 'inline-source-map',
-  output: {
-    filename: '[name].main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    fallback: {
-      "path": false,
-      "fs": false
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader','sass'],
-      },
-      {
-        test: /\.(html)$/i,
-        use: ['html-loader'],
-      },
-      {
-        test: /\.(svg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      }
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/temp.html',
-      title: 'Check today\'s weather..',
-      favicon: 'src/img/umbrella.svg'
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(dotenv.config().parsed)
-    })
-  ]
-}
+	mode: 'development',
+	entry: {
+		app: './src/app.js',
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+	},
+	output: {
+		filename: '[name].main.js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '',
+	},
+	resolve: {
+		fallback: {
+			path: false,
+			fs: false,
+
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader', 'sass'],
+			},
+			{
+				test: /\.(html)$/i,
+				use: ['html-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
+	plugins: [
+		new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+		new HtmlWebpackPlugin({
+			inject: true,
+			template: 'src/temp.html',
+			title: 'Check today\'s weather..',
+			favicon: 'src/img/umbrella.svg',
+		}),
+		// new Dotenv({
+		// 	silent: true, // hide any warnings
+		// }),
+		// new webpack.DefinePlugin({
+		// 	'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+		// }),
+		// new webpack.EnvironmentPlugin({
+		// 	NODE_ENV: 'development',
+		// }),
+	],
+};
